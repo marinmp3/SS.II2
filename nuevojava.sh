bc5ef7528bad93ad0e0c77fc6c2018ec105d32c2
min <145747769+marinmp3@users.noreply.github.com>
2024-01-31 09:12:40 +0100

    Update nuevojava.sh

--- nuevojava.sh
+++ nuevojava.sh
@@ -10,3 +10,16 @@ public class App{
 		System.out.println(\"Hola mundo!\");
 	}
 }">>nuevojava/src/App.java
+
+# PARA INTRODUCIRLO POR ARGUMENTO
+#!/bin/bash
+mkdir nuevojava
+mkdir nuevojava/bin
+mkdir nuevojava/src
+mkdir nuevojava/lib
+printf "
+public class App{
+	public static void main(String[] args){
+		System.out.println(\"Hola mundo!\");
+	}
+}">>$1/src/App.java
