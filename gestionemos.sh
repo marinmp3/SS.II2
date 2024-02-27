08237cbb719cd7e97b4844786b71adf1200998b2
min <145747769+marinmp3@users.noreply.github.com>
2024-02-27 14:15:06 +0100

    Add files via upload

--- gestionemos.sh
+++ gestionemos.sh
@@ -1,37 +1,139 @@
 #/bin/bash
-if [$1 == "-m" || $1 == "--manual"]
+# orden=$1
+directorio_creado=""
+salida=true
+orden=$1
+for arg in $@
+do
+    fichero="./gestionemos.log"
+    touch $fichero
+    printf "%-20s %-20s %-20s %-10s %-10s %-10s %-10s %-10s %-10s\n" `date "+%F %T %z"` $fichero $orden $salida >> $fichero 
+
+
+if [ $1 == "-m" ] || [ $1 == "--manual" ]
     then
-    man ls
-elif [$1 == "-d" || $1 == "--dir"]
+    echo "-m || --manual: Muestra un manual de uso del script."
+    echo "-d || --dir: Crea directorios."
+    echo "-p || --process: Lista recursivamente los procesos en funcionamiento."
+    echo "-l || --list: Lista los ficheros del directorio especificado."
+    echo "-k || --kill: Mata el proceso especificado."
+    echo "-b || --backup: Crea y comprime una copia del directorio HOME."
+
+elif [ $1 == "-d" ] || [ $1 == "--dir" ]
+then
+    shift
+    argumento=$1    
+    if [ $argumento == "-d" ] || [ $argumento == "--dir" ]
     then
-    mkdir $#
-    for argumento in $@
-    if [$argumento == "+"]
+        shift
+        argPosterior=$1
+        echo "seleccionaste -d o --dir"
+    else
+        if [ $argumento = "+" ]
+        then
+            if [ -z "$directorio_creado" ]
+            then
+                echo "No se puede usar '+' sin haber creado un directorio previamente"
+            fi
+        else
+        if [ -z "$directorio_creado" ]
+        then
+            mkdir -p "$argumento"
+            directorio_creado="$argumento"
+        else
+            mkdir -p "$directorio_creado/$arg"
+            fi
+        fi
+    fi
+
+
+elif [ $1 == "-p" ] || [ $1 == "--process" ]
+then
+    shift
+    user=$1
+    touch "procesos_$user_`date`.log"
+    top -u $user >> procesos_"$user"_"$date".log
+    
+
+elif [ $1 == "-l" ] || [ $1 == "--list" ]
+then
+    shift
+    while $1 == "-l" 
     do
-        mkdir $argumento
+        shift
+        nuevaorden=$1
+        echo "Introduce --list o --log: "
+        if [[ $nuevaorden == "--list" ]]
+        then
+            if [ $# -gt 1 ]
+            then 
+                shift
+                dir_arg=$1
+                touch "ficheros_$dir_arg_`date`.log"
+                ls -a $dir_ar >> "ficheros_$dir_arg_`date`.log"
+                for fichero in $dir_arg
+                do
+                    echo El directorio $dir_arg contiene $# ficheros
+                done
+            else
+                touch "ficheros_HOME_`date`.log"
+                ls -a ~ >> "ficheros_HOME_`date`.log"
+            fi
+        fi
     done
-        
+    
+ 
+elif [ $1 == "-k" ] || [ $1 == "--kill" ]
+then
+    shift
+    PID=$1
+    read -r "¿Quieres proseguir con la operación? (se eliminará el proceso $PID). Introduce S/n: "
+    shift
+    respuesta=$1
+    if [ $respuesta == "S" ]
+    then
+        kill $PID
+        echo El proceso se ha eliminado correctamente.
+    else
+        echo No se ha eliminado el proceso.
+    fi
+
+elif [ $1 == "-b" ] || [ $1 == "--backup" ]
+then
+    shift
+    touch backup_file
+    backup_file="backup_$(date +%Y%m%d_%H%M%S).zip"
+    gzip -r "backup_file" ~/*
+
+    if [ $? -eq 0 ]
+    then
+        echo "Copia de seguridad creada correctamente: $backup_file"
+    else
+        echo "Error al crear la copia de seguridad."
+    fi
+
+elif [ $1 == "-l" ] || [ $1 == "--log" ]
+then
+    shift
+    while $1 == "-l" 
+    do
+        shift
+        nuevaorden=$1
+        echo "Introduce -list o -log: "
+        if [[ $1 == "--log" ]]
+        then
+            `date` >> $fichero
+        fi
+    done
+    
+    
+
+else
+    echo Argumento no reconocido.
+    salida=false
 
 
+fi
+done
 
-
-
-
-# ● -m o --manual: Muestra un manual de uso del script.
-# ● -d o --dir: Crea directorios basados en los argumentos dados. Si un argumento es el
-# carácter ‘+’, los directorios a partir de ese argumento se crean dentro del último
-# directorio creado.
-# ○ Ejemplo: Si proporcionas los argumentos dir1 dir2 + dir3, se crearán los
-# directorios dir1 y dir2, y dentro de dir2 se creará dir3.
-# ● -p o --process: Lista todos los procesos en funcionamiento del usuario
-# especificado como argumento en un archivo
-# procesos_{usuario}_{fecha_hora}.log y muestra por pantalla el total de
-# procesos en funcionamiento.
-# ● -l o --list: Lista todos los ficheros del directorio especificado como argumento en
-# un archivo ficheros_{directorio}_{fecha_hora}.log y muestra el total de
-# ficheros por pantalla. Si no se proporciona ningún argumento, se realiza el listado
-# sobre el directorio ${HOME}.
-# ● -k o --kill: Mata el proceso especificado como argumento (PID), solicitando
-# confirmación antes de hacerlo (comando read).
-# ● -b o --backup: Crea una copia del directorio ${HOME} y lo comprime en un archivo
-# zip (comando zip) con el nombre backup_{fecha_hora}.zip .
+      
